f3b0aecb29c69138cd23c5be41e14c1bb9cc6316
= <=>
2019-11-19 09:14:32 +0700

    unguided pwa

---
+++ precache-manifest.89e0fd118456aec456900806f68ef972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6373a56445cd8ef75cc4",
+    "url": "/css/chunk-vendors.c893da24.css"
+  },
+  {
+    "revision": "a8f2141a99ee82ed2cd477fcb00d9598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e27b7e4d6da8c8eee7f",
+    "url": "/js/app.0188c2a5.js"
+  },
+  {
+    "revision": "6373a56445cd8ef75cc4",
+    "url": "/js/chunk-vendors.914150cd.js"
+  },
+  {
+    "revision": "d95e5133445b9b9be1c436f6215005ec",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
